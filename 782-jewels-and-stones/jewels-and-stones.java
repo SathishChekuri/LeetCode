ebca2d8e5d895bea1ac8571908c7ec647608ecc5
SathishChekuri <chekurisathish41@gmail.com>
2025-11-04 09:59:57 +0530

    Time: 1 ms (59.64%) | Memory: 42.8 MB (7.06%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int c=0;
+        for(int i=0;i<stones.length();i++){
+            if(jewels.contains(stones.charAt(i)+"")) c++;
+        }
+        return c;
+    }
+}
