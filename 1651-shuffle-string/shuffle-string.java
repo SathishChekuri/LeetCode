d8ecfda97412541d93e8114856c75ef9b1f9bd26
SathishChekuri <chekurisathish41@gmail.com>
2025-11-07 11:52:42 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (5.45%) - LeetSync

--- 1651-shuffle-string/shuffle-string.java
+++ 1651-shuffle-string/shuffle-string.java
@@ -1,11 +1,10 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
-        StringBuilder  r=new StringBuilder("");
+        //StringBuilder  r=new StringBuilder("");
         char c[]=new char[s.length()];
         for(int i=0;i<indices.length;i++){
             c[indices[i]]=s.charAt(i);
         }
-        r.append(c);
-        return r.toString();
+        return new String(c);
     }
 }
