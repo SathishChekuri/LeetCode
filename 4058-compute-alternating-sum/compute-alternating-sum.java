448185922747a8756f863509ce89499b7d9d1656
SathishChekuri <chekurisathish41@gmail.com>
2025-11-13 21:43:57 +0530

    Time: 1 ms (94.40%) | Memory: 46.3 MB (5.85%) - LeetSync

--- 4058-compute-alternating-sum/compute-alternating-sum.java
+++ 4058-compute-alternating-sum/compute-alternating-sum.java
@@ -1,9 +1,10 @@
 class Solution {
     public int alternatingSum(int[] nums) {
         int s=0;
-        for(int i=0;i<nums.length;i++){
-            s+=(i%2==0)?nums[i]:(-nums[i]);
-        }
+        for(int i=0;i<nums.length;i+=2) s+=nums[i];
+           // s+=(i%2==0)?nums[i]:(-nums[i]);
+       // }
+         for(int i=1;i<nums.length;i+=2) s-=nums[i];
         return s;
     }
 }
