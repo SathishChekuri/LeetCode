ebbc2ee210e0851f36eb58093c6870658172cfcf
SathishChekuri <chekurisathish41@gmail.com>
2025-11-14 10:51:59 +0530

    Time: 1 ms (100.00%) | Memory: 45.7 MB (25.58%) - LeetSync

---
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String removeTrailingZeros(String num) {
+        int c=0;
+        for(int i=num.length()-1;i>=0;i--){
+            if(num.charAt(i)!='0') break;
+            c++;
+        }
+        return num.substring(0,num.length()-c);
+    }
+}
