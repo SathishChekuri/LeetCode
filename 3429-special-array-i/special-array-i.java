80601228865dcc1d6031a46a59d25264ae1e7217
SathishChekuri <chekurisathish41@gmail.com>
2026-01-17 15:28:17 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (93.24%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -5,11 +5,7 @@ class Solution {
         }
         
         for(int i=1;i<nums.length;i++){
-            if((nums[i]%2==0 && nums[i-1]%2!=0) || (nums[i]%2!=0 && nums[i-1]%2==0)){
-                continue;
-            }else{
-                return false;
-            }
+            if(nums[i]%2==nums[i-1]%2)    return false;
         }
         return true;
     }
