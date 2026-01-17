fa1a6f108f9c24c2e8d6ad385ee172214331685e
SathishChekuri <chekurisathish41@gmail.com>
2026-01-17 15:27:06 +0530

    Time: 1 ms (58.50%) | Memory: 45.1 MB (39.57%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -1,7 +1,15 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]%2==nums[i+1]%2) return false;
+        if(nums.length==1){
+            return true;
+        }
+        
+        for(int i=1;i<nums.length;i++){
+            if((nums[i]%2==0 && nums[i-1]%2!=0) || (nums[i]%2!=0 && nums[i-1]%2==0)){
+                continue;
+            }else{
+                return false;
+            }
         }
         return true;
     }
