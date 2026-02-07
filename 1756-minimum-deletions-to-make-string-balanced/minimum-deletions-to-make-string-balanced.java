1413fb6e53de3dc69763f7f0f38b52ff8e83c551
SathishChekuri <chekurisathish41@gmail.com>
2026-02-07 07:41:23 +0530

    Time: 22 ms (67.11%) | Memory: 47.9 MB (30.43%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,18 +1,13 @@
 class Solution {
     public int minimumDeletions(String s) {
-        int ra=0,lb=0,mn=s.length();
-        if((s.length()<=1)||!(s.contains("a")&&s.contains("b"))) return 0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='a'){
-                    ra++;
+        int ans=0,cnt=0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'b') cnt++;
+            else if (cnt>0){
+                cnt--;
+                ans++;
             }
         }
-        for(int i=0;i<s.length();i++){
-            mn=Math.min(ra+lb,mn);
-            if(s.charAt(i)=='b') lb++;
-            else if(s.charAt(i)=='a') ra--;
-            mn=Math.min(ra+lb,mn);
-        }
-        return mn;
+        return ans;
     }
 }
