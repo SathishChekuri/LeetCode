454aa930f83077faa7da912bf0cbd455b9363cba
SathishChekuri <chekurisathish41@gmail.com>
2026-03-12 07:46:09 +0530

    Time: 20 ms (83.14%), Space: 56.3 MB (87.62%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i=0,j=people.length-1,c=0;
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                i++;
+                j--;
+                c++;
+            }
+            else {
+                j--;
+                c++;
+            }
+        }
+        return c;
+    }
+}
