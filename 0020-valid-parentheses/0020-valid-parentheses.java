2f29ae000dcd89e2fad3e53773d91e3fc5e82a22
SathishChekuri <chekurisathish41@gmail.com>
2026-03-14 16:18:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,11 +3,9 @@ class Solution {
         Stack<Character> stk=new Stack<>();
         for(char c:s.toCharArray()){
             if(c=='('||c=='{'||c=='[') stk.push(c);
-            else if(c==')'||c=='}'||c==']'){
-                if(stk.empty()) return false;
+            else if(stk.empty()) return false;
             else if((c==')'&&stk.peek()=='(')||(c=='}'&&stk.peek()=='{')||(c==']'&&stk.peek()=='[')) stk.pop();
-            else return false;
-           }
+            //else return false;
         }
            if(stk.size()==0) return true;
            return false;
