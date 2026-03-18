835a0a8ce473a68449364248f30f524216366e26
SathishChekuri <chekurisathish41@gmail.com>
2026-03-18 07:44:51 +0530

    Time: 7 ms (50.81%), Space: 161.3 MB (63.71%) - LeetHub

---
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countSubmatrices(int[][] grid, int k) {
+        int m=grid[0].length;
+        int n=grid.length;
+        int c=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+               if(i>0) grid[i][j]+=grid[i-1][j];
+               if(j>0) grid[i][j]+=grid[i][j-1];
+               if(i>0&&j>0) grid[i][j]-=grid[i-1][j-1];
+                if(grid[i][j]<=k){
+                  c++;
+                }
+                else break;
+            }
+        }
+        return c;
+    }
+}
