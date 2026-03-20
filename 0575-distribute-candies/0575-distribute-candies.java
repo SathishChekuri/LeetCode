5852729ad9c9379473bb202e8cf6652b98d52c6f
SathishChekuri <chekurisathish41@gmail.com>
2026-03-20 12:01:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -2,7 +2,7 @@ class Solution {
     public int distributeCandies(int[] candyType) {
         HashSet<Integer> hs=new HashSet<>();
         for(int i:candyType) hs.add(i);
-        if(hs.size()>candyType.length/2) return candyType.length/2;
-        return hs.size();
+       // if(hs.size()>candyType.length/2) return;
+        return Math.min(candyType.length/2,hs.size());
     }
 }
