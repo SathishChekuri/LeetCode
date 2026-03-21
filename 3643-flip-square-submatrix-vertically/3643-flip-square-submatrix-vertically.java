3a064afb6ee43ca19c67bfc3c1b52d80dd837f1a
SathishChekuri <chekurisathish41@gmail.com>
2026-03-21 08:26:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.java
+++ 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.java
@@ -1,18 +1,15 @@
 class Solution {
     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
         int rmx=x+k-1;
-        int cmx=y+k;
-        while(y<cmx){
-            int i=x,j=rmx;
-            while(i<j){
-                int t=grid[i][y];
-                grid[i][y]=grid[j][y];
-                grid[j][y]=t;
-                i++;
-                j--;
+        while(x<rmx){
+               for(int i=y;i<y+k;i++){
+                int t=grid[x][i];
+                grid[x][i]=grid[rmx][i];
+                grid[rmx][i]=t;
+               }
+                x++;
+                rmx--;
             }
-            y++;
-        }
         return grid;
     }
 }
