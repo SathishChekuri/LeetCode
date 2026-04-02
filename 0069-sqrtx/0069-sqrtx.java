57afc534f42f7b6a1a8aed805e2ed3e8ebaf201d
SathishChekuri <chekurisathish41@gmail.com>
2026-04-02 07:52:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,7 +1,13 @@
 class Solution {
     public int mySqrt(int x) {
-        long i;
-        for(i=0;i*i<=x;i++);
-        return (int)i-1;
+        long i=0,j=x/2+1;
+        while(i<=j){
+          long mid=(i+j)/2;
+          if((int)mid*mid==x) return (int)mid;
+          else if((int)mid*mid>x) j=mid-1;
+          else i=mid+1;
+
+        }
+        return (int)j;
     }
 }
