c58109c183b5fc6152d7ea7e61f7c4f0b696d809
SathishChekuri <chekurisathish41@gmail.com>
2026-05-02 07:12:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.java
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -1,23 +1,29 @@
 class Solution {
     public int rotatedDigits(int n) {
-        int c=0;
-        for(int i=2;i<=n;i++){
-            int num=i;
-            boolean valid=true;
-            boolean rotate=false;
-            while(num>0){
-                int d=num%10;
-                num/=10;
-                if(d==3||d==7||d==4){
-                    valid=false;
-                    break;
-                }
-                else if(d==2||d==5||d==6||d==9){
-                    rotate=true;
-                }
+        int count = 0;
+        
+        for (int i = 1; i<= n; i++) {
+            if(isGood(i)) {
+                count++;
             }
-            if(valid&&rotate) c++;
         }
-        return c;
+        return count;
+        
+    }
+    private boolean isGood(int num) {
+        boolean isDifferent = false;
+        
+        while(num > 0) {
+            int digit = num % 10;
+            num /= 10;
+            
+            if (digit == 3 || digit == 4 ||digit == 7) {
+                return false;
+            }
+            if (digit == 2 || digit == 5 || digit == 6 || digit == 9) {
+                isDifferent = true;
+            }
+        }
+        return isDifferent;
     }
 }
