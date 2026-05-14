91795b2ed793e8a73d6bf8ad145c44f2105519ba
SathishChekuri <chekurisathish41@gmail.com>
2026-05-14 08:57:50 +0530

    Time: 1 ms (91.05%), Space: 45.1 MB (31.81%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean isGood(int[] nums) {
-        int [] arr=new int[200];
+        int [] arr=new int[201];
         int mx=0;
         for(int i:nums){
             mx=Math.max(i,mx);
-            arr[i-1]++;
+            arr[i]++;
         }
+        if(nums.length!=mx+1) return false;
         int flg=1;
         for(int i=1;i<mx;i++){
-            if(arr[i-1]!=1) return false;
+            if(arr[i]!=1) return false;
         }
-        if(arr[mx-1]!=2) return false;
+        if(arr[mx]!=2) return false;
         return true;
     }
 }
