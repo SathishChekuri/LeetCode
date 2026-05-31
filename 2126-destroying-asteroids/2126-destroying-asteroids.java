e1dc4fcaf803fbd696cd06dbcbd8c780ee65bb9d
SathishChekuri <chekurisathish41@gmail.com>
2026-05-31 10:07:27 +0530

    Time: 24 ms (94.23%), Space: 107.6 MB (16.4%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -2,7 +2,9 @@ class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
         Arrays.sort(asteroids);
         long mas=mass;
+        int maxast=asteroids[asteroids.length-1];
         for(int i:asteroids){
+            if(mas>=maxast) return true;
             if(i<=mas) mas+=i;
             else return false;
         }
