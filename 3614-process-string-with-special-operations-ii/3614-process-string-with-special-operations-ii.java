0a4b7938ada56d6275edb9cda7e5657dd30369c0
SathishChekuri <chekurisathish41@gmail.com>
2026-06-17 07:25:28 +0530

    Time: 28 ms (88.46%), Space: 47.3 MB (96.15%) - LeetHub

--- 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.java
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.java
@@ -1,48 +1,27 @@
 class Solution {
     public char processStr(String s, long k) {
-        int n = s.length();
-        long[] lengths = new long[n + 1];
-
-        for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
-            long length = lengths[i];
-            if (Character.isLowerCase(ch)) {
-                lengths[i + 1] = length + 1;
-            } else if (ch == '*') {
-                lengths[i + 1] = Math.max(0, length - 1);
-            } else if (ch == '#') {
-                lengths[i + 1] = length * 2;
-            } else if (ch == '%') {
-                lengths[i + 1] = length;
+        long m=0;
+        for(char c:s.toCharArray()){
+            if(c=='*') m=Math.max(0,m-1);
+            else if(c=='#') m<<=1;
+            else if(c!='%') m+=1;
+        }
+        if(k>=m) return '.';
+        for(int i=s.length()-1;i>=0;i--){
+            char c=s.charAt(i);
+            if(c=='*') m+=1;
+            else if(c=='#'){
+                m/=2;
+                if(k>=m) k-=m;
+            }
+            else if(c=='%'){
+                k=m-1-k;
+            }
+            else{
+                m--;
+                if(m==k) return s.charAt(i);
             }
         }
-
-        long finalLength = lengths[n];
-
-        if (k < 0 || k >= finalLength) {
-            return '.';
-        }
-
-        for (int i = n - 1; i >= 0; i--) {
-            char ch = s.charAt(i);
-            long beforeLen = lengths[i];
-            long afterLen = lengths[i + 1];
-
-            if (Character.isLowerCase(ch)) {
-                if (k == afterLen - 1) {
-                    return ch;
-                }
-            } else if (ch == '*') {
-               continue;
-            } else if (ch == '#') {
-                if (k >= beforeLen) {
-                    k -= beforeLen;
-                }
-            } else if (ch == '%') {
-                k = beforeLen - 1 - k;
-            }
-        }
-
-        return '.';       
+        return '.';
     }
 }
