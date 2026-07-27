b80752ea392bb8d8530d1a8c534ed58c2d13d20e
SathishChekuri <chekurisathish41@gmail.com>
2026-07-27 20:19:04 +0530

    Time: 8 ms (91.68%), Space: 49.4 MB (14.68%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<int []> arr=new ArrayList<>();
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        int pa=intervals[0][0];
-        int pb=intervals[0][1];
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        List<int []> ar=new ArrayList<>();
+        int st=intervals[0][0],end=intervals[0][1];
         for(int i=1;i<intervals.length;i++){
-            int [] ar=intervals[i];
-            if(pb>=ar[0]){
-                pb=Math.max(ar[1],pb);
-                    continue;
+            int arr[]=intervals[i];
+            if(end>=arr[0]){
+                end=Math.max(end,arr[1]);
             }
-            else {
-                arr.add(new int[]{pa,pb});
-                pa=ar[0];
-                pb=ar[1];
+            else{
+                ar.add(new int[]{st,end});
+                st=arr[0];
+                end=arr[1];
             }
         }
-        arr.add(new int[]{pa,pb});
-        return arr.toArray(new int[arr.size()][]);
+        ar.add(new int[]{st,end});
+        int a[][]=new int[ar.size()][];
+        int i=0;
+        for(int val[]:ar) a[i++]=val;
+        return a;
     }
 }
