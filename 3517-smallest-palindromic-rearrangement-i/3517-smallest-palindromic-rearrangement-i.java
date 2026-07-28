a6b8cd3f4ae0f967dc6c0d75b3c8ad68e2179f7b
SathishChekuri <chekurisathish41@gmail.com>
2026-07-28 07:29:45 +0530

    Time: 29 ms (73.49%), Space: 48.1 MB (57.43%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -5,16 +5,15 @@ class Solution {
             arr[s.charAt(i)-'a']++;
         }
         StringBuilder sb=new StringBuilder();
-        char md='0';
+        char mid='0';
         for(int i=0;i<26;i++){
             int t=arr[i]/2;
             char c=(char)('a'+i);
-            if(arr[i]%2==1) md=c;
+            if(arr[i]%2==1) mid=c;
             while(t-->0) sb.append(c);
         }
-        int mid=sb.length();
-        String rev=sb.toString();
-        if(md!='0') rev+=md;
-        return rev+sb.reverse().toString();
+        String firstHalf=sb.toString();
+        if(mid!='0') firstHalf+=mid;
+        return firstHalf+sb.reverse().toString();
     }
 }
