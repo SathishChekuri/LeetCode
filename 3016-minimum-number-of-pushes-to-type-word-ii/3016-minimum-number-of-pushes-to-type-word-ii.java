c40d94db31d59b49d11b81163a9d67df6827d550
SathishChekuri <chekurisathish41@gmail.com>
2026-07-31 07:42:08 +0530

    Time: 9 ms (94.22%), Space: 48.3 MB (17.34%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,18 +1,15 @@
 class Solution {
     public int minimumPushes(String word) {
-        Map<Character,Integer> hs=new HashMap<>();
+        int [] arr=new int[26];
         for(int i=0;i<word.length();i++){
-            hs.put(word.charAt(i),hs.getOrDefault(word.charAt(i),0)+1);
+            arr[word.charAt(i)-'a']++;
         }
-        List<Map.Entry<Character,Integer>> entrylist=new ArrayList<>(hs.entrySet());
-        entrylist.sort((entry1,entry2)->entry2.getValue().compareTo(entry1.getValue()));
-        int press=1,count=0,ans=0;
-        for(Map.Entry<Character,Integer> ent:entrylist){
-            count++;
-            if(count>8&&press==1) press=2;
-            else if(count>16&&press==2) press=3;
-            else if(count>24&&press==3) press=4;
-            ans+=ent.getValue()*press;
+        int ans=0,indx=0;
+        Arrays.sort(arr);
+        for(int i=25;i>=0;i--){
+            if(arr[i]==0) break;
+            ans+=arr[i]*(indx/8+1);
+            indx++;
         }
         return ans;
     }
