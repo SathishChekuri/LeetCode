e1f3b2980ffe085a9b836b61fff9fd5f15393c24
SathishChekuri <chekurisathish41@gmail.com>
2026-08-02 08:03:25 +0530

    Time: 9 ms (13.82%), Space: 46.6 MB (19.1%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,17 @@
 class Solution {
+    int dp[][];
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n=piles.length;
+        dp=new int[n][n];
+        for(int []r:dp) Arrays.fill(r,-1);
+        helper(0,n-1,piles);
+        return dp[0][n-1]>0;
     }
+   int helper(int i,int j,int[] piles){
+        if(i>j) return 0;
+        if(i==j) return piles[i];
+        if(dp[i][j]!=-1) return dp[i][j];
+        return dp[i][j]=Math.max(piles[i]-helper(i+1,j,piles),piles[j]-helper(i,j-1,piles));
+    }
+
 }
