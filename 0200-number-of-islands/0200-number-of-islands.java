2da257d5037ed2664c3685a383d3c136253e74c3
SathishChekuri <chekurisathish41@gmail.com>
2026-08-18 19:09:37 +0530

    Time: 5 ms (28.52%), Space: 52.2 MB (59.2%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -6,30 +6,23 @@ class Solution {
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]=='1'&&!visited[i][j]){
                     c++;
-                    bfs(grid,visited,i,j);
+                    dfs(grid,visited,i,j);
                 }
             }
         }
         return c;
     }
-    void bfs(char [][]grid,boolean[][] visited,int r,int c){
-        Queue<int[]> q=new LinkedList<>();
-         visited[r][c]=true;
-        q.add(new int[]{r,c});
-        while(!q.isEmpty()){
-            int arr[]=q.remove();
-            r=arr[0];
-            c=arr[1];
+    void dfs(char [][]grid,boolean[][] visited,int r,int c){
+          visited[r][c]=true;
             for(int i=-1;i<=1;i++){
                 for(int j=-1;j<=1;j++){
                     if(i==j||i+j==0) continue;
                     int nr=r+i;
                     int nc=c+j;
                     if(nr<grid.length&&nr>=0&&nc>=0&&nc<grid[0].length&&grid[nr][nc]=='1'&&!visited[nr][nc]){
-                        q.add(new int[]{nr,nc});
                          visited[nr][nc]=true;
+                          dfs(grid,visited,nr,nc);
                     }
-                }
             }
         }
     }
