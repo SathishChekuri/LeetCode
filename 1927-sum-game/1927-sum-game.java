cb6568ec3b37ae4090da923e65f3fac89c21f26a
SathishChekuri <chekurisathish41@gmail.com>
2026-08-23 08:32:30 +0530

    Time: 9 ms (29.93%), Space: 47.1 MB (41.61%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -3,14 +3,16 @@ class Solution {
         int lsum=0,rsum=0,lq=0,rq=0;
         int n=num.length();
         for(int i=0;i<n/2;i++){
-            if(num.charAt(i)=='?') lq++;
-            else lsum+=num.charAt(i)-'0';
-            if(num.charAt(n/2+i)=='?') rq++;
-            else rsum+=num.charAt(n/2+i)-'0';
+            char c=num.charAt(i);
+            char ch=num.charAt(n/2+i);
+            if(c=='?') lq++;
+            else lsum+=c-'0';
+            if(ch=='?') rq++;
+            else rsum+=ch-'0';
     }
     int diff=lsum-rsum;
     int qdiff=lq-rq;
     if(qdiff%2!=0) return true;
-    return diff!=-9*(qdiff/2);
+    return diff+9*(qdiff/2)!=0;
     }
 }
