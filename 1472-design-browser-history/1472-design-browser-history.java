d2e0e79b393218f0a67a576cc6b717fb75b67312
SathishChekuri <chekurisathish41@gmail.com>
2026-08-30 08:33:42 +0530

    Time: 48 ms (87.12%), Space: 52.6 MB (49.81%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.java
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -1,33 +1,38 @@
 class BrowserHistory {
-    ArrayList<String> ar;
-    int c=0;
+    Node curr;
     public BrowserHistory(String homepage) {
-        ar=new ArrayList<>();
-        ar.add(homepage);
+       curr=new Node(homepage);
         
     }
     
     public void visit(String url) {
-        ar.add(c+1,url);
-        c++;
-       for(int i=c+1;i<ar.size();) ar.remove(i);
-       System.out.println(ar.toString());
+       curr.next=new Node(url);
+       curr.next.prev=curr;
+       curr=curr.next;
     }
     
     public String back(int steps) {
-        if(c-steps<0){
-            c=0;
+        while(steps-->0){
+            if(curr.prev==null) break;
+            curr=curr.prev;
         }
-        else c-=steps;
-        return ar.get(c);
+        return curr.val;
     }
     
     public String forward(int steps) {
-        if(c+steps>ar.size()-1){
-            c=ar.size()-1;
-        }
-        else c+=steps;
-        return ar.get(c);
+       while(steps-->0){
+        if(curr.next==null) break;
+        curr=curr.next;
+       }
+        return curr.val;
+    }
+}
+class Node{
+    String val;
+    Node next;
+    Node prev;
+    Node(String val){
+        this.val=val;
     }
 }
 
