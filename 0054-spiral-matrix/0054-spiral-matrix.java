0c63a208ec0c9403955e41e8d1587ba7a010e21f
SathishChekuri <chekurisathish41@gmail.com>
2026-09-01 07:44:42 +0530

    Time: 0 ms (100%), Space: 43.1 MB (44.02%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,22 +1,44 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> arr=new ArrayList<>();
-        int n=matrix.length-1,m=matrix[0].length-1;
-        int r=0,c=0;
-        while(r<=n&&c<=m){
-            for(int j=c;j<=m;j++) arr.add(matrix[r][j]);
-            r++;
-            for(int i=r;i<=n;i++) arr.add(matrix[i][m]);
-            m--;
-            if(r<=n){
-            for(int i=m;i>=c;i--) arr.add(matrix[n][i]);
-            n--;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        boolean[][] visited = new boolean[n][m];
+        List<Integer> ar = new ArrayList<>();
+        int r = 0, c = 0;
+        ar.add(matrix[r][c]);
+        visited[r][c] = true;
+        while (true) {
+            if (c + 1 < m && !visited[r][c + 1]) {
+                while (c + 1 < m && !visited[r][c + 1]) {
+                    ar.add(matrix[r][c + 1]);
+                    c++;
+                    visited[r][c] = true;
+                }
+                continue;
+            } else if (r + 1 < n && !visited[r + 1][c]) {
+                while (r + 1 < n && !visited[r + 1][c]) {
+                    ar.add(matrix[r + 1][c]);
+                    r++;
+                    visited[r][c] = true;
+                }
+                continue;
+            } else if (c - 1 >= 0 && !visited[r][c - 1]) {
+                while (c - 1 >= 0 && !visited[r][c - 1]) {
+                    ar.add(matrix[r][c - 1]);
+                    c--;
+                    visited[r][c] = true;
+                }
+                continue;
+            } else if (r - 1 >= 0 && !visited[r - 1][c]) {
+                while (r - 1 >= 0 && !visited[r - 1][c]) {
+                    ar.add(matrix[r - 1][c]);
+                    r--;
+                    visited[r][c] = true;
+                }
+                continue;
             }
-            if(c<=m){
-            for(int i=n;i>=r;i--) arr.add(matrix[i][c]);
-            c++;
-            }
+            break;
         }
-        return arr;
+        return ar;
     }
 }
