6d6484eec0d7d0d2f75decad2d83017265449bd9
SathishChekuri <chekurisathish41@gmail.com>
2026-09-03 07:24:45 +0530

    Time: 5 ms (83.33%), Space: 121.7 MB (44.2%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int mxod=-1,mnod=-1,mxev=-1,mnev=-1;
+        int mnod=-1,mnev=-1;
         for(int i:nums1){
             if(i%2==1){
-                if(mxod<i||mxod==-1) mxod=i;
                 if(mnod==-1||mnod>i) mnod=i;
             }
             else{
                 if(mnev==-1||mnev>i) mnev=i;
-                if(mxev==-1||mxev<i) mxev=i;
             }
         }
         return (mnev>mnod||mnod==-1||mnev==-1);
