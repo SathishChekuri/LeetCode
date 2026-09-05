1d54b2c802c898d307089b013ed1c0bb7666042a
SathishChekuri <chekurisathish41@gmail.com>
2026-09-05 21:12:38 +0530

    Time: 2 ms (41.62%), Space: 47.5 MB (31.65%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,19 +1,46 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        boolean [] visited=new boolean[isConnected.length];
-     int c=0;
-     for(int i=0;i<visited.length;i++){
-            if(!visited[i]){
-                c++;
-                dfs(i,visited,isConnected);
+        int n=isConnected.length;
+        disjoint_set dsj=new disjoint_set(n);
+       for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i!=j&&isConnected[i][j]==1){
+                int u=dsj.find(i),v=dsj.find(j);
+                if(u==v) continue;
+                if(dsj.rank[u]>dsj.rank[v]){
+                    dsj.parent[v]=u;
+                }
+                else if(dsj.rank[v]>dsj.rank[u]){
+                    dsj.parent[u]=v;
+                }
+                else{
+                    dsj.rank[u]++;
+                    dsj.parent[v]=u;
+                }
             }
         }
-        return c;
+       }
+       return dsj.find_p();
     }
-    void dfs(int node,boolean[] visited,int [][]graf){
-        visited[node]=true;
-        for(int i=0;i<visited.length;i++){
-            if(!visited[i]&&graf[node][i]==1) dfs(i,visited,graf);
+}
+class disjoint_set{
+    int parent[];
+    int rank[];
+    disjoint_set(int n){
+        parent=new int [n];
+        rank=new int[n];
+        for(int i=0;i<n;i++) parent[i]=i;
+    }
+    int find(int i){
+        if(parent[i]==i) return parent[i];
+        parent[i]=find(parent[i]);
+        return parent[i];
+    }
+    int find_p(){
+        int c=0;
+        for(int i=0;i<parent.length;i++){
+            if(parent[i]==i) c++;
         }
+        return c;
     }
 }
