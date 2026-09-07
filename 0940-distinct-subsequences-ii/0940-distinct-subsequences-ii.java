9089e32c8e3b463b7fdeb47e859c87a244613ee4
SathishChekuri <chekurisathish41@gmail.com>
2026-09-07 07:05:04 +0530

    Time: 4 ms (70.57%), Space: 42.9 MB (96.1%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int distinctSubseqII(String s) {
+        long MOD = 1000000007L;
+        long[] dp = new long[s.length() + 1];
+        long[] last = new long[26];
+
+        dp[0] = 1;
+
+        for(int i = 1; i <= s.length(); i++) {
+            int c = s.charAt(i - 1) - 'a';
+
+            dp[i] = (2 * dp[i - 1] - last[c] + MOD) % MOD;
+
+            last[c] = dp[i - 1];
+        }
+
+        return (int)((dp[s.length()] - 1 + MOD) % MOD); 
+    }
+}
